480c63f5027dc9d0510ff3e5b2113936375333c2
Arisu_04 <156810186+harish040120@users.noreply.github.com>
2025-05-14 01:26:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,4 +115,4 @@ def get_violations():
     })
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), allow_unsafe_werkzeug=True)
